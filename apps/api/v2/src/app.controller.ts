55644a84a54a3d64d932978c14c0f2b0f51df100
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-15 21:42:00 -0300

    fix env path

--- apps/api/v2/src/app.controller.ts
+++ apps/api/v2/src/app.controller.ts
@@ -1,19 +1,15 @@
 import { Controller, Get, Version, VERSION_NEUTRAL } from "@nestjs/common";
-import { ApiTags as DocsTags } from "@nestjs/swagger";
-import * as fs from "fs";
+import { ApiExcludeController as DocsExcludeController, ApiTags as DocsTags } from "@nestjs/swagger";
 
-// import { getEnv } from "src/env";
+import { getEnv } from "./env";
 
 @Controller()
 @DocsTags("Health - development only")
-// @DocsExcludeController(getEnv("NODE_ENV") === "production")
+@DocsExcludeController(getEnv("NODE_ENV") === "production")
 export class AppController {
   @Get("health")
   @Version(VERSION_NEUTRAL)
-  getHealth(): string {
-    // retorna todos os arquivos desta pasta
-    const path = JSON.stringify(fs.readdirSync(__dirname));
-
-    return path;
+  getHealth(): "OK" {
+    return "OK";
   }
 }
