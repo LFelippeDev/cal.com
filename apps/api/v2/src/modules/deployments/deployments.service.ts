a11bb1482cc5b636b97eecb7a0174b402749b80d
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-17 10:29:20 -0300

    Fix dont use config service

--- apps/api/v2/src/modules/deployments/deployments.service.ts
+++ apps/api/v2/src/modules/deployments/deployments.service.ts
@@ -1,10 +1,7 @@
+import { getEnv } from "@/env";
 import { Injectable } from "@nestjs/common";
 
 import { DeploymentsRepository } from "../deployments/deployments.repository";
-
-const CACHING_TIME = 86400000; // 24 hours in milliseconds
-
-const getLicenseCacheKey = (key: string) => `api-v2-license-key-url-${key}`;
 
 type LicenseCheckResponse = {
   valid: boolean;
@@ -13,25 +10,29 @@ type LicenseCheckResponse = {
 export class DeploymentsService {
   constructor(private readonly deploymentsRepository: DeploymentsRepository) {}
 
-  async checkLicense() {
-    return true;
+  private is_e2e = getEnv("IS_E2E");
+  private licenseKey = getEnv("CALCOM_LICENSE_KEY");
+  private licenseKeyUrl = getEnv("GET_LICENSE_KEY_URL");
 
-    let licenseKey = "";
+  async checkLicense() {
+    if (this.is_e2e) {
+      return true;
+    }
+    let licenseKey = this.licenseKey;
 
     if (!licenseKey) {
       /** We try to check on DB only if env is undefined */
       const deployment = await this.deploymentsRepository.getDeployment();
-      licenseKey = deployment?.licenseKey || "";
+      licenseKey = deployment?.licenseKey ?? undefined;
     }
 
     if (!licenseKey) {
       return false;
     }
-    const licenseKeyUrl = "" + `?key=${licenseKey}`;
+    const licenseKeyUrl = this.licenseKeyUrl + `?key=${licenseKey}`;
 
     const response = await fetch(licenseKeyUrl, { mode: "cors" });
     const data = (await response.json()) as LicenseCheckResponse;
-    const cacheKey = getLicenseCacheKey(licenseKey);
     return data.valid;
   }
 }
