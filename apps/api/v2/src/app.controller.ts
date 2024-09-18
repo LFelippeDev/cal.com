8828e9d21d1f07e7bed728fb0731c7b1d027fecc
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-18 09:44:55 -0300

    Add root route

--- apps/api/v2/src/app.controller.ts
+++ apps/api/v2/src/app.controller.ts
@@ -5,11 +5,17 @@ import { getEnv } from "./env";
 
 @Controller()
 @DocsTags("Health - development only")
-@DocsExcludeController(getEnv("NODE_ENV") === "production")
+// @DocsExcludeController(getEnv("NODE_ENV") === "production")
 export class AppController {
   @Get("health")
   @Version(VERSION_NEUTRAL)
   getHealth(): "OK" {
     return "OK";
+  }
+
+  @Get("/")
+  @Version(VERSION_NEUTRAL)
+  getRoot(): JSON {
+    return JSON.parse(`{"message":"Welcome to Cal.com API V2 - docs are at https://developer.cal.com/api}`);
   }
 }
