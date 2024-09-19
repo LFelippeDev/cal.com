c70f3994cc29801a92a4137b4e55b4d0a72dc540
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-19 15:18:41 -0300

    unchange tests

--- apps/api/v2/src/ee/bookings/bookings.module.ts
+++ apps/api/v2/src/ee/bookings/bookings.module.ts
@@ -3,13 +3,13 @@ import { Module } from "@nestjs/common";
 import { BillingModule } from "../../modules/billing/billing.module";
 import { OAuthClientRepository } from "../../modules/oauth-clients/oauth-client.repository";
 import { OAuthFlowService } from "../../modules/oauth-clients/services/oauth-flow.service";
-// import { PrismaModule } from "../../modules/prisma/prisma.module";
+import { PrismaModule } from "../../modules/prisma/prisma.module";
 import { TokensModule } from "../../modules/tokens/tokens.module";
 import { TokensRepository } from "../../modules/tokens/tokens.repository";
 import { BookingsController } from "../bookings/controllers/bookings.controller";
 
 @Module({
-  imports: [TokensModule, BillingModule],
+  imports: [PrismaModule, TokensModule, BillingModule],
   providers: [TokensRepository, OAuthFlowService, OAuthClientRepository],
   controllers: [BookingsController],
 })
