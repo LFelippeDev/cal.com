39c2e899af581a613458b91280b8752a4fbfdc2f
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-20 11:25:14 -0300

    Fix test

--- apps/api/v2/src/modules/teste/teste.controller.ts
+++ apps/api/v2/src/modules/teste/teste.controller.ts
@@ -1,14 +1,20 @@
+import { supabase } from "@/config/supabase";
+import { API_VERSIONS_VALUES } from "@/lib/api-versions";
 import { Controller, Get } from "@nestjs/common";
 
-import { SupabaseService } from "../supabase/supabase.service";
-
-@Controller("v2/teste")
+@Controller({
+  path: "/v2/teste",
+  version: API_VERSIONS_VALUES,
+})
 export class TesteController {
-  constructor(private readonly supabaseService: SupabaseService) {}
-
-  @Get()
+  @Get("/")
   async getBookings(): Promise<any> {
-    const data = await this.supabaseService.getData("Booking");
+    const { data, error } = await supabase.from("Booking").select("*");
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
     return { message: "Dados obtidos com sucesso!", data };
   }
 }
--- apps/api/v2/src/modules/teste/teste.module.ts
+++ apps/api/v2/src/modules/teste/teste.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 
-import { SupabaseModule } from "../supabase/supabase.module";
 import { TesteController } from "./teste.controller";
 
 @Module({
-  imports: [SupabaseModule],
   controllers: [TesteController],
 })
 export class TesteModule {}
