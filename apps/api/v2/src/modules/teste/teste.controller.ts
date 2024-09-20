07c191ecc2b68050232d8b779832f42801e1d962
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-20 11:43:49 -0300

    dont use @ in imports

--- apps/api/v2/src/modules/teste/teste.controller.ts
+++ apps/api/v2/src/modules/teste/teste.controller.ts
@@ -1,6 +1,7 @@
-import { supabase } from "@/config/supabase";
 import { API_VERSIONS_VALUES } from "@/lib/api-versions";
 import { Controller, Get } from "@nestjs/common";
+
+import { supabase } from "../../config/supabase";
 
 @Controller({
   path: "/v2/teste",
