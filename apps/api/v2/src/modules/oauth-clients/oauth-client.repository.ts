0e30d1fc2d1aacdfead3fecef3255dc1066c0691
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-24 14:27:26 -0300

    Create oauth user test 6

--- apps/api/v2/src/modules/oauth-clients/oauth-client.repository.ts
+++ apps/api/v2/src/modules/oauth-clients/oauth-client.repository.ts
@@ -3,13 +3,13 @@ import type { PlatformOAuthClient } from "@prisma/client";
 
 import type { CreateOAuthClientInput } from "@calcom/platform-types";
 
+import { supabase } from "../../config/supabase";
 import { JwtService } from "../jwt/jwt.service";
 
 @Injectable()
 export class OAuthClientRepository {
   constructor(private jwtService: JwtService) {}
 
-  // TODO: PrismaWriteService
   async createOAuthClient(organizationId: number, data: CreateOAuthClientInput) {
     // return this.dbWrite.prisma.platformOAuthClient.create({
     //   data: {
@@ -19,14 +19,18 @@ export class OAuthClientRepository {
     //   },
     // });
   }
-  // TODO: PrismaReadService
-  async getOAuthClient(clientId: string): Promise<PlatformOAuthClient | null> {
-    // return this.dbRead.prisma.platformOAuthClient.findUnique({
-    //   where: { id: clientId },
-    // });
 
-    // tirar essa linha
-    return null;
+  async getOAuthClient(clientId: string): Promise<PlatformOAuthClient | null> {
+    const { data, error } = (await supabase
+      .from("PlatformOAuthClient")
+      .select("*")
+      .eq("id", clientId)
+      .limit(1)
+      .single()) as any;
+
+    if (error) return null;
+
+    return data as PlatformOAuthClient;
   }
 
   // TODO: PrismaReadService
