bcc827cbba67faedd9a8230ef3f2e4cda8b7793d
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-24 13:16:12 -0300

    Create oauth user test 4

--- apps/api/v2/src/modules/oauth-clients/controllers/oauth-client-users/oauth-client-users.controller.ts
+++ apps/api/v2/src/modules/oauth-clients/controllers/oauth-client-users/oauth-client-users.controller.ts
@@ -80,7 +80,7 @@ export class OAuthClientUsersController {
       `Creating user with data: ${JSON.stringify(body, null, 2)} for OAuth Client with ID ${oAuthClientId}`
     );
     const data = (await this.oauthRepository.getOAuthClient(oAuthClientId)) as any;
-    console.log("asap createUser client", JSON.stringify(client, null, 2));
+    // console.log("asap createUser client", JSON.stringify(client, null, 2));
 
     // const isPlatformManaged = true;
     // const { user, tokens, message } = await this.oAuthClientUsersService.createOauthClientUser(
