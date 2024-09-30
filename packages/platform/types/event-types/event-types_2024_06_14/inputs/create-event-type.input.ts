4455f8fb3fada9469b9cad7f68ff27ecabd60cda
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-30 15:56:46 -0300

    Create oauth user test 63

--- packages/platform/types/event-types/event-types_2024_06_14/inputs/create-event-type.input.ts
+++ packages/platform/types/event-types/event-types_2024_06_14/inputs/create-event-type.input.ts
@@ -33,23 +33,19 @@ export const CREATE_EVENT_DESCRIPTION_EXAMPLE =
 
 export class CreateEventTypeInput_2024_06_14 {
   @IsInt()
-  @IsOptional()
   @Min(1)
   userId?: number;
 
   @IsInt()
-  @IsOptional()
   @Min(1)
   @DocsProperty({ example: CREATE_EVENT_LENGTH_EXAMPLE })
   lengthInMinutes?: number;
 
   @IsString()
-  @IsOptional()
   @DocsProperty({ example: CREATE_EVENT_TITLE_EXAMPLE })
   title?: string;
 
   @IsString()
-  @IsOptional()
   slug?: string;
 
   @IsOptional()
